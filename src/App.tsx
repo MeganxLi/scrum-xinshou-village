e94cb3c420f51ecc00383489180ef4882e1c6795
Meiyi <ant19950601@gmail.com>
2022-11-28 11:17:33 +0800

    :memo: add Loading page gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React from "react";
-import "./App.scss";
+import React, { useEffect, useState } from "react";
+import "./styles/App.scss";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? null : (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
+            </a>
+          </header>
+        </div>
+      )}
+    </>
   );
 }
 
