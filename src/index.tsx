ed28d3cfd5ba5d95f4333187edeb1eafda09c30c
Meiyi <ant19950601@gmail.com>
2022-12-06 23:40:21 +0800

    :bug: React 18 StrictMode rendering breaks by react-beautiful-dnd

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import StepContextProvider from "./components/StepProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <StepContextProvider>
-      <App />
-    </StepContextProvider>
-  </React.StrictMode>
+  <StepContextProvider>
+    <App />
+  </StepContextProvider>
 );
