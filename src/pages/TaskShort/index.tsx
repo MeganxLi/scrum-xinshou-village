1c242e742029e4fbe36ed4464ae03e20c1b1eec1
Meiyi <ant19950601@gmail.com>
2022-12-19 23:16:54 +0800

    :lipstick: add TaskShort page style and animate

--- src/pages/TaskShort/index.tsx
+++ src/pages/TaskShort/index.tsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import DropItem from "../../components/DropItem";
 import { TaskShortItem } from "../../constants/EnumType";
+import { useStep } from "../../context/StepContext";
 
 const TaskShort = () => {
+  const { nextStep } = useStep();
   const [items, setItems] = useState<TaskPriorityItemsType>(JSON.parse(JSON.stringify(TaskShortItem)));
   const [totalScoreSum, setTotalScoreSum] = useState(0);
 
   const onDragEnd = (event: any) => {
     const { source, destination } = event;
-    console.log(source, destination);
 
     // dropped outside the list
     if (!destination) {
@@ -47,14 +48,26 @@ const TaskShort = () => {
   };
 
   return (
-    <section id="TaskShort">
-      <img className="banner" src={process.env.PUBLIC_URL + "/images/TaskShort_banner.png"} />
-      <img className="img-bla" src={process.env.PUBLIC_URL + "/images/bla.gif"} />
-      <img className="img-bla" src={process.env.PUBLIC_URL + "/images/bla.gif"} />
-      <p className="directions">將產品待辦清單，拖拉過去短衝清單，短衝是有限制可完成的 Point。</p>
+    <section id="TaskShort" className="flex-center">
+      <div
+        className="animate__fadeOut duration animation-forwards"
+        style={{ animationDelay: 1.5 + "s" }}
+      >
+        <img className="banner" src={process.env.PUBLIC_URL + "/images/TaskShort_banner.png"} />
+        <img className="img-bla" src={process.env.PUBLIC_URL + "/images/bla.gif"} />
+        <img className="img-bla" src={process.env.PUBLIC_URL + "/images/bla.gif"} />
+      </div>
+      <p
+        className="directions animate__fadeIn duration animation-backwards"
+        style={{ animationDelay: 2.25 + "s" }}
+      >
+        將產品待辦清單，拖拉過去短衝清單，短衝是有限制可完成的 Point。
+      </p>
 
-      <div className="pad">
-        <img className="img-pad" src={process.env.PUBLIC_URL + "/images/pad_2.png"} />
+      <div
+        className="pad animate__slideInUp duration animation-backwards"
+        style={{ animationDelay: 1.5 + "s" }}
+      >
         <div className="pad-content">
           <div className="pad-title">
             <p>Projects / TT / 人才招募系統 </p>
@@ -85,33 +98,33 @@ const TaskShort = () => {
             }}>
               短衝待辦清單 <sub>(Product Backlog)</sub>
             </h5>
-            <div className="drop-sort-list">
-              <Droppable droppableId="sprintList">
-                {(provided) => (
-                  <div
-                    className="drop-list"
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                  >
-                    {items.sprintList.map((item: DropItemType, index: number) => {
-                      return (
-                        <DropItem item={item} index={index} key={item.id} />
-                      );
-                    })}
+            <Droppable droppableId="sprintList">
+              {(provided) => (
+                <div
+                  className="drop-sort-list"
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {items.sprintList.map((item: DropItemType, index: number) => {
+                    return (
+                      <DropItem item={item} index={index} key={item.id} />
+                    );
+                  })}
 
-                    {provided.placeholder}
+                  {provided.placeholder}
 
-                  </div>
+                </div>
 
-                )}
+              )}
 
-              </Droppable>
-            </div>
+            </Droppable>
 
           </DragDropContext>
           <div className="pad-action">
-            <img src={process.env.PUBLIC_URL + "/images/go.png"}
-              style={{ visibility: totalScoreSum === 16 ? "visible" : "hidden" }}
+            <img
+              src={process.env.PUBLIC_URL + "/images/go.png"}
+              style={{ visibility: totalScoreSum === 16 ? "visible" : "hidden", cursor: "pointer" }}
+              onClick={nextStep}
             />
             <Undo
               strokeWidth={4}
