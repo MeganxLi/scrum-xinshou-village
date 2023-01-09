afd7a69a66c52f4caa196ffdb1eeed59148856b0
Mei Yi <ant19950601@gmail.com>
2023-01-10 00:24:50 +0800

    :memo: update TaskPriority drop sprintList order, and error animate

--- src/constants/EnumType.ts
+++ src/constants/EnumType.ts
@@ -1,81 +1,85 @@
 import uuid from "react-uuid";
 
 export enum DialogIconButton {
-  Undo = "Undo",
-  DownOne = "DownOne",
+   Undo = "Undo",
+   DownOne = "DownOne",
 }
 
 export const TaskPriorityItem: TaskItemsType = {
-  candidate: [
-    {
-      title: "後台職缺管理功能",
-      subtitle: "（資訊上架、下架、顯示應徵者資料）",
-      id: uuid(),
-    },
-    {
-      title: "會員系統",
-      subtitle: "（登入、註冊、權限管理）",
-      id: uuid(),
-    },
-    {
-      title: "線上履歷編輯器",
-      id: uuid(),
-    },
-    {
-      title: "前台職缺列表",
-      subtitle: "（職缺詳細內容、發送應徵意願）",
-      id: uuid(),
-    },
-  ],
-  sprintList: [],
+   candidate: [
+      {
+         title: "後台職缺管理功能",
+         subtitle: "（資訊上架、下架、顯示應徵者資料）",
+         id: uuid(),
+         priority: 4,
+      },
+      {
+         title: "會員系統",
+         subtitle: "（登入、註冊、權限管理）",
+         id: uuid(),
+         priority: 1,
+      },
+      {
+         title: "線上履歷編輯器",
+         id: uuid(),
+         priority: 2,
+      },
+      {
+         title: "前台職缺列表",
+         subtitle: "（職缺詳細內容、發送應徵意願）",
+         id: uuid(),
+         priority: 3,
+      },
+   ],
+   sprintList: [],
 };
 
 export const TaskShortItem: TaskItemsType = {
-  candidate: [
-    {
-      title: "後台職缺管理功能",
-      subtitle: "（資訊上架、下架、顯示應徵者資料）",
-      id: uuid(),
-      score: 8,
-    },
-    {
-      title: "會員系統",
-      subtitle: "（登入、註冊、權限管理）",
-      id: uuid(),
-      score: 8,
-    },
-    {
-      title: "前台職缺列表",
-      subtitle: "（職缺詳細內容、發送應徵意願）",
-      id: uuid(),
-      score: 5,
-    },
-    {
-      title: "線上履歷編輯器",
-      id: uuid(),
-      score: 15,
-    },
-  ],
-  sprintList: [],
+   candidate: [
+      {
+         title: "後台職缺管理功能",
+         subtitle: "（資訊上架、下架、顯示應徵者資料）",
+         id: uuid(),
+         score: 8,
+      },
+      {
+         title: "會員系統",
+         subtitle: "（登入、註冊、權限管理）",
+         id: uuid(),
+         score: 8,
+      },
+      {
+         title: "前台職缺列表",
+         subtitle: "（職缺詳細內容、發送應徵意願）",
+         id: uuid(),
+         score: 5,
+      },
+      {
+         title: "線上履歷編輯器",
+         id: uuid(),
+         score: 15,
+      },
+   ],
+   sprintList: [],
 };
 
 export const TaskFlowItem: TaskItemsType = {
-  candidate: [
-    {
-      title: "短衝自省會議",
-      subtitle: "(Retro)",
-      id: uuid(),
-    },
-    {
-      title: "每日站立會議",
-      subtitle: "(Daily Scrum)",
-      id: uuid(),
-    },
-    {
-      title: "短衝檢視會議",
-      subtitle: "(Sprint Review)",
-      id: uuid(),
-    },
-  ],
-  sprintList: [],
+   candidate: [
+      {
+         title: "短衝自省會議",
+         subtitle: "(Retro)",
+         id: uuid(),
+      },
+      {
+         title: "每日站立會議",
+         subtitle: "(Daily Scrum)",
+         id: uuid(),
+      },
+      {
+         title: "短衝檢視會議",
+         subtitle: "(Sprint Review)",
+         id: uuid(),
+      },
+   ],
+   sprintList: [],
 };
